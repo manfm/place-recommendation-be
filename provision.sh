f2a6841c2f961e41541844f98545a4dc8c873fc9
Martin Chapcak <chapcak.m@gmail.com>
2015-11-28 22:20:58 +0900

    Generated rails app with postgresql

--- provision.sh
+++ provision.sh
@@ -4,6 +4,7 @@ cd /vagrant
 
 sudo -E apt-get install -y git
 sudo -E apt-get install -y libsqlite3-dev
+sudo -E apt-get install -y postgresql postgresql-contrib libpq-de
 sudo -E apt-get install -y nodejs
 
 echo "cd /vagrant" >> /home/vagrant/.bashrc
